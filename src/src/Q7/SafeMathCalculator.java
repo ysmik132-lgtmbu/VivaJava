b443008fb1b1932c8bf117fd5d5975b4a8276c93
ysmik132-lgtmbu <ysmik132@gmail.com>
2026-06-18 08:59:38 +0545

    SafeMathCalculator.java

---
+++ src/src/Q7/SafeMathCalculator.java
@@ -0,0 +1,26 @@
+//Bhudev Katawal 116237 BIT sec "P"
+
+public class SafeMathCalculator {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 0;
+
+        try {
+
+            int result = a / b;
+            // May throw exception
+
+            System.out.println(result);
+
+        }
+
+        catch(ArithmeticException e) {
+            // Handles divide-by-zero
+
+            System.out.println(
+                    "Cannot divide by zero");
+        }
+    }
+}
