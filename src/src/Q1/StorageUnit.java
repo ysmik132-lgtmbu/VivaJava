afbf4526f09f564a61eb510ada5f282b889bfe87
ysmik132-lgtmbu <ysmik132@gmail.com>
2026-06-18 07:52:32 +0545

    StorageUnit.java

--- src/src/Q1/StorageUnit.java
+++ src/src/Q1/StorageUnit.java
@@ -1,7 +1,6 @@
-//Bhudev Katawal 116237 BIT sec "P".
+//Bhudev Katawal 116237 BIT sec "P"
 
-
-abstract class StorageUnit {
+abstract class StorageUnit { // Abstract parent class
 
     int id;
     double capacity;
@@ -12,22 +11,19 @@ abstract class StorageUnit {
     }
 }
 
-interface Refrigerated {
+interface Refrigerated { // Interface declaration
 
-    void adjustTemp(int temperature);
+    void adjustTemp(int t);
 }
 
-class ColdBox extends StorageUnit implements Refrigerated {
+class ColdBox extends StorageUnit implements Refrigerated { // Inheritance + Interface
 
     ColdBox(int id, double capacity) {
-        super(id, capacity);
+        super(id, capacity); // Calls parent constructor
     }
 
-    @Override
-    public void adjustTemp(int temperature) {
-        System.out.println(
-                "Temperature adjusted to "
-                        + temperature + "°C");
+    public void adjustTemp(int t) {
+        System.out.println("Temperature adjusted to " + t + "°C");
     }
 }
 
@@ -35,15 +31,11 @@ public class SmartWarehouse {
 
     public static void main(String[] args) {
 
-        ColdBox box =
-                new ColdBox(101, 500);
+        ColdBox box = new ColdBox(101, 500); // Object creation
 
-        System.out.println(
-                "Storage ID: " + box.id);
+        System.out.println("ID: " + box.id);
+        System.out.println("Capacity: " + box.capacity);
 
-        System.out.println(
-                "Capacity: " + box.capacity);
-
-        box.adjustTemp(4);
+        box.adjustTemp(4); // Interface method call
     }
 }
