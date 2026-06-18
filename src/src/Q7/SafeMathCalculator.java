6476aa8a2f94019783789df6688310d2c5867357
ysmik132-lgtmbu <ysmik132@gmail.com>
2026-06-18 09:12:30 +0545

    Update SafeMathCalculator.java

--- src/src/Q7/SafeMathCalculator.java
+++ src/src/Q7/SafeMathCalculator.java
@@ -1,25 +1,40 @@
 //Bhudev Katawal 116237 BIT sec "P"
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class SafeMathCalculator {
 
     public static void main(String[] args) {
 
-        int a = 10;
-        int b = 0;
+        Scanner sc = new Scanner(System.in);
 
         try {
 
+            System.out.print("Enter first number: ");
+            int a = sc.nextInt();
+
+            System.out.print("Enter second number: ");
+            int b = sc.nextInt();
+
             int result = a / b;
-            // May throw exception
+            // Division operation
 
-            System.out.println(result);
-
+            System.out.println("Result = " + result);
         }
 
         catch(ArithmeticException e) {
-            // Handles divide-by-zero
+            // Handles divide by zero
 
-            System.out.println( "Cannot divide by zero");
+            System.out.println("Cannot divide by zero");
         }
+
+        catch(InputMismatchException e) {
+            // Handles invalid input
+
+            System.out.println("Enter integers only");
+        }
+
+        sc.close();
     }
 }
